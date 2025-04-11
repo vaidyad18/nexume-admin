9505f57828de2457ccb7c3215c738158acaac4ee
Vaidya Dandriyal <vaidyadandriyal04@gmail.com>
2025-04-12 01:04:14 +0530

    done

--- config/database.js
+++ config/database.js
@@ -1,32 +1,19 @@
-const path = require('path');
 
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME' ),
-        user: env('DATABASE_USERNAME' ),
-        password: env('DATABASE_PASSWORD'),
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      debug:false
-    }
-  };
-  
-
-  return {
-    
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      connectionString: env("DATABASE_URL"),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", false),
+      },
+      schema: env("DATABASE_SCHEMA", "public"),
     },
-  };
-};
+    debug: false,
+  },
+});
